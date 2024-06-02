fe4bfca73cd4ed59785080d0912a6a4aac08fd53
Alaa Seada <alaaseada87@gmail.com>
2024-06-02 21:12:06 +0300

    Add simple maven and simple node apps

---
+++ docs/CI-CD/Jenkins/practice-multi-agent-multi-stage/simple-maven-app/src/test/java/com/example/app/HelloWorldTest.java
@@ -0,0 +1,12 @@
+package com.example.app;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+public class HelloWorldTest {
+    @Test
+    public void testMain() {
+        HelloWorld.main(null);
+        assertTrue(true);
+    }
+}
